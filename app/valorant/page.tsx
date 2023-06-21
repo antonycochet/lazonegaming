54eaa36c15841b331d73c2d9a1acb732786efa71
DenneulinThomas <t.denneulin@apodispharma.com>
2023-06-21 08:15:17 +0200

    :sparkles: First riot api call

--- app/valorant/page.tsx
+++ app/valorant/page.tsx
@@ -1,3 +1,11 @@
-export default function ValorantPage() {
+import {api} from "@/services/riot-api/riot-api";
+
+const getData = async () => {
+    return await api("riot/account/v1/accounts/by-riot-id/jesaispasqui%20INT/EUW");
+}
+
+export default async function ValorantPage() {
+    const data = await getData()
+
     return <h1 className="text-5xl text-white">Valorant</h1>
 }
---
+++ constants.ts
@@ -0,0 +1,5 @@
+export const constants = {
+    valorantPlayers: [
+        "Jesaispasqui INT#EUW"
+    ]
+}
